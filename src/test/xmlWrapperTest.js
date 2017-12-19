34afe1479e2c939df41d2210dc28ed7bf97e8236
Dominik <dominik.preikschat@rub.de>
2017-12-19 21:28:40 +0100

    Changed the entire data handling as preparation of remote changes.

--- src/test/xmlWrapperTest.js
+++ src/test/xmlWrapperTest.js
@@ -10,7 +10,7 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
     describe('insert test', function () {
 
         it('insert a single character', function () {
-            let doc = new Doc(initialDoc)
+            let doc = new Doc(initialDoc);
             let xmlWrapper = new XmlWrapper(doc);
             let delta = new Delta().insert('a');
             xmlWrapper.quillTextChanged(delta);
@@ -18,7 +18,7 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
         });
 
         it('insert a full block of characters (xml block should be filled with the insert)', function () {
-            let doc = new Doc(initialDoc)
+            let doc = new Doc(initialDoc);
             let xmlWrapper = new XmlWrapper(doc);
             let delta = new Delta().insert(randomString(xmlWrapper.MAX_BLOCK_SIZE));
             xmlWrapper.quillTextChanged(delta);
@@ -26,7 +26,7 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
         });
 
         it('insert more than one full block', function () {
-            let doc = new Doc(initialDoc)
+            let doc = new Doc(initialDoc);
             let xmlWrapper = new XmlWrapper(doc);
             let delta = new Delta().insert(randomString(xmlWrapper.MAX_BLOCK_SIZE +randomUnsignedInt(1,100)));
             xmlWrapper.quillTextChanged(delta);
@@ -34,14 +34,28 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
         });
 
         it('insert random characters multiple times', function () {
-            let doc = new Doc(initialDoc)
-            let xmlWrapper = new XmlWrapper(doc);
-            let deltaResult = new Delta();
-            for(let i = 0; i < 10; i++){
+            let doc = null;
+            let xmlWrapper = null;
+            let deltaResult = null;
+            for(let i = 0; i < 1000; i++){
+                doc = new Doc(initialDoc);
+                xmlWrapper = new XmlWrapper(doc);
+                deltaResult = new Delta();
                 let delta = new Delta().insert(randomString(Math.floor(Math.random() * 10 + 1)));
                 xmlWrapper.quillTextChanged(delta);
                 deltaResult = deltaResult.compose(delta);
             }
+            assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert);
+        });
+
+        it('insert character somewhere', function () {
+            let doc = new Doc(initialDoc);
+            let xmlWrapper = new XmlWrapper(doc);
+            let deltaResult = new Delta().insert(randomString(xmlWrapper.MAX_BLOCK_SIZE +randomUnsignedInt(1,100)));
+            xmlWrapper.quillTextChanged(deltaResult);
+            let delta = new Delta().retain(randomUnsignedInt(1,xmlWrapper.MAX_BLOCK_SIZE)).insert("0");
+            xmlWrapper.quillTextChanged(delta);
+            deltaResult = deltaResult.compose(delta);
             assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert);
         });
     });
@@ -49,7 +63,7 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
     describe('delete test', function () {
 
         it("delete a character at pos 2", function () {
-            let doc = new Doc(initialDoc)
+            let doc = new Doc(initialDoc);
             let xmlWrapper = new XmlWrapper(doc);
             let deltaResult = new Delta().insert("Thiis");
             let delta = new Delta().retain(2).delete(1);
@@ -59,27 +73,33 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
             assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert);
         });
 
-        it("delete multiple characters at a random start pos", function () {
-            let doc = new Doc(initialDoc)
-            let xmlWrapper = new XmlWrapper(doc);
-            let text = randomString(randomUnsignedInt(1,100));
-            let deltaResult = new Delta().insert(text);
-            let startPos = randomUnsignedInt(0,text.length - 1);
-            let deleteCount = randomUnsignedInt(1,text.length);
-            if(deleteCount > text.length - startPos)
-                deleteCount = text.length - startPos;
-            let delta = new Delta().retain(startPos).delete(deleteCount);
-            xmlWrapper.quillTextChanged(deltaResult);
-            xmlWrapper.quillTextChanged(delta);
-            deltaResult = deltaResult.compose(delta);
-            assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert);
+        it("delete multiple characters at a random start pos multiple times", function () {
+            for(var i = 0; i < 1000; i++) {
+                let doc = new Doc(initialDoc)
+                let xmlWrapper = new XmlWrapper(doc);
+                let text = randomString(randomUnsignedInt(1, 100));
+                let deltaResult = new Delta().insert(text);
+                let startPos = randomUnsignedInt(0, text.length - 1);
+                let deleteCount = randomUnsignedInt(1, text.length);
+                if (deleteCount > text.length - startPos)
+                    deleteCount = text.length - startPos;
+                let delta = new Delta().retain(startPos).delete(deleteCount);
+                xmlWrapper.quillTextChanged(deltaResult);
+                xmlWrapper.quillTextChanged(delta);
+                deltaResult = deltaResult.compose(delta);
+                if(deltaResult.ops[0] === undefined)
+                    assert.equal(xmlWrapper.documentText, '');
+                else
+                    assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert);
+            }
         });
     });
 
     describe('replace test', function () {
+
         it("replace a random character (100 times)", function () {
             for(var i = 0; i < 100; i++){
-                let doc = new Doc(initialDoc)
+                let doc = new Doc(initialDoc);
                 let xmlWrapper = new XmlWrapper(doc);
                 let text = randomString(randomUnsignedInt(50,100));
                 let input = new Delta().insert(text);
@@ -90,20 +110,13 @@ describe('XMLWrapperTest - Text output check, only checks if the output is corre
                     .insert(randomString(randomUnsignedInt(1,50)))
                     .delete(randomUnsignedInt(1,20));
                 //TODO put into function, check for each operation
-                var message = "\nAssertionError:" + i + "Expected: " + deltaResult.compose(delta).ops[0].insert + "\n"
-                    + "Actual   : " + xmlWrapper.documentText + "\n" +
-                    "input    : " + input.ops[0].insert + "\n" +
-                    "replacement: {retain: " + delta.ops[0].retain  + " insert: " + delta.ops[1].insert;
-                if (delta.ops[2])
-                    message += " delete: " +  delta.ops[2].delete + "\n";
                 xmlWrapper.quillTextChanged(input);
-
                 xmlWrapper.quillTextChanged(delta);
+                let msg = generateInfoMessage(input, deltaResult.compose(delta),
+                    xmlWrapper.documentText, delta);
                 deltaResult = deltaResult.compose(delta);
-
-                assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert, message);
+                assert.equal(xmlWrapper.documentText, deltaResult.ops[0].insert, msg);
             }
-
         });
     });
 });
@@ -130,3 +143,24 @@ var randomString = function (length) {
 var randomUnsignedInt = function(min, max){
     return Math.floor(Math.random() * max + min);
 };
+
+var generateInfoMessage = function (inputDelta, expectedDelta, actual, delta){
+    var msg = "\ninput   : " + deltaPrint(inputDelta) + "\n"
+    + "expected: " + deltaPrint(expectedDelta) + "\n"
+    + "actual  : " + actual + "\n"
+    + "delta   : " + deltaPrint(delta);
+    return msg;
+};
+
+var deltaPrint = function (delta) {
+    var msg = "";
+    for(let i = 0; i < delta.ops.length; i++){
+        if(delta.ops[i].retain)
+            msg += "retain: " + delta.ops[i].retain + ", ";
+        if(delta.ops[i].insert)
+            msg += "insert: " + delta.ops[i].insert + ", ";
+        if(delta.ops[i].delete)
+            msg += "delete: " + delta.ops[i].delete + ", ";
+    }
+    return msg;
+}
